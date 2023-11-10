c316e849b7406836d338f59d88678e540a90a0ef
KingoBoiii <kingodk@outlook.dk>
2023-11-10 11:29:22 +0100

    Changed design

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,135 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:tuple/tuple.dart';
-
-class ToDo {
-  String todo;
-  bool completed = false;
-  
-  ToDo(this.todo);
-}
-
-class ToDoList {
-  String name;
-  IconData icon;
-  List<ToDo> todos;
-  
-  ToDoList(this.name, this.icon, this.todos);
-}
-
-final List<ToDoList> todoLists = <ToDoList>[
-  ToDoList(
-    'Shopping',
-    Icons.shopping_bag,
-    <ToDo>[
-      ToDo('Rugbrød'),
-      ToDo('Småkage')
-    ] 
-  ),
-  ToDoList(
-    'Gaming',
-    Icons.gamepad,
-    <ToDo>[
-      ToDo('Arma 3')
-    ] 
-  )
-];
 
 void main() {
-  runApp(const MainApp());
+  runApp(const TodoApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: TodoTabLayout()
+      title: 'To-Do List', 
+      home: TodoList()
     );
   }
 }
 
-class TodoTabLayout extends StatelessWidget {
-  const TodoTabLayout({super.key});
+class TodoList extends StatefulWidget {
+  const TodoList({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _TodoListState createState() => _TodoListState();
+}
+
+class Todo {
+  Todo({required this.name, required this.completed});
+  String name;
+  bool completed;
+}
+
+
+class _TodoListState extends State<TodoList> {
+  List<Todo> todos = <Todo>[];
+  static int s_NextId = 0;
+  final TextEditingController textFieldController = TextEditingController();
+
+  void _addTodo(String todo) {
+    setState(() {
+      todos.add(Todo(name: todo, completed: false));
+    });
+  }
+
+  void _deleteTodo(Todo todo) {
+    setState(() {
+      todos.remove(todo);
+    });
+  }
+
+  void _updateTodo(Todo todo, bool? newState) {
+    setState(() {
+      todo.completed = newState ?? false;
+    });
+  }
+
+  void _clearAllTodo() {
+    setState(() {
+      todos.clear();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final List<Tuple2<Tab, Widget>> tabs = todoLists
-      .expand((element) => element.todos.map((e) => Tuple2(
-          Tab(
-            icon: Icon(element.icon),
-            child: Text(element.name)
-          ),
-          Center(
-            child: Text("${e.todo}: ${e.completed}")
-          )
-        )))
-      .toList();
-
-    return DefaultTabController(
-      length: tabs.length,
-      child: Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Tabs Demo'),
-          bottom: TabBar(
-            isScrollable: true,
-            tabs: tabs.map((e) => e.item1).toList()
+          title: const Text('To-Do List'),
+          actions: [
+            IconButton(
+              onPressed: () => _clearAllTodo(),
+              tooltip: 'Clear all Todos',
+              icon: const Icon(Icons.clear)
+            )
+          ],
+        ),
+        body: ListView(
+          children: todos.map((e) => 
+            ListTile(
+              title: Text(e.name),
+              leading: Checkbox(
+                onChanged: (newState) => _updateTodo(e, newState),
+                value: e.completed
+              ),
+              trailing: IconButton(
+                onPressed: () => _deleteTodo(e),
+                icon: const Icon(Icons.delete)
+              ),
+            )
+          ).toList(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _showMyDialog, // _addTodo("Todo #${++s_NextId}"),
+          tooltip: 'Add a new Todo',
+          child: const Icon(Icons.add)
+        ),
+      );
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return CupertinoAlertDialog(
+          title: const Text('AlertDialog Title'),
+          content: CupertinoTextField(
+            controller: textFieldController
           ),
-        ),
-        drawer: const Drawer(
-          child: SafeArea( 
-            child: Text('Hello World!') 
-          ),
-        ),
-        body: TabBarView(
-          children: tabs.map((e) => e.item2).toList(),
-        ),
-      ),
-    );
+          actions: <Widget>[
+            CupertinoDialogAction(
+              isDestructiveAction: true,
+              child: const Text('Don\'t Add'),
+              onPressed: () {
+                Navigator.pop(context);
+              }
+            ),
+            CupertinoDialogAction(
+              isDefaultAction: true,
+              child: const Text('Add'),
+              onPressed: () {
+                if(textFieldController.text.isEmpty) {
+                  return;
+                }
+
+                _addTodo(textFieldController.text);
+                textFieldController.text = '';
+
+                Navigator.pop(context);
+              }
+            )
+          ],
+        );
+      });
   }
 }
